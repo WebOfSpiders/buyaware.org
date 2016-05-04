7c2551c6eee2fcc1186b8e43065e30961b578548
rico500 <eric.mhr.brunner@gmail.com>
2016-05-04 23:49:06 +0200

    Populate repository.

---
+++ home/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'home'
+urlpatterns = [
+    #home-page
+    url(r'^$', views.home, name='home'),
+    url(r'^en$', views.home_en, name='home_en'),
+    #url(r'^/de/$', views.home_de, name='home_de'),
+]
---
+++ home/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+
+# Create your views here.
+
+def home(request):
+
+    return redirect(reverse('home:home_en'))
+
+def home_en(request):
+
+    return render(request, 'home/home_en.html')
+
+#def home_de(request):
+#
+#   return render(request, 'home/home_de.html')
